b17cfc7e62c99b197dd1a4b8176ba3127458882e
Siddharth-Reddy7 <siddarthreddy8310@gmail.com>
2024-09-18 09:06:57 +0530

    Add files via upload

---
+++ classtype.R
@@ -0,0 +1,31 @@
+#Student details
+"Student details"
+print("Siddharth Reddy")
+print("1AY23AI112")
+print("CGPA IS 7.0")
+a<-2
+7->b
+c<-a+b
+c
+?setwd
+help("reserved")
+#numeric
+a=1
+class(a)
+typeof(a)
+c="Hello"
+class(c)
+typeof(c)
+d='Hello'
+class(d)
+typeof(d)
+e='H'
+class(e)
+typeof(e)
+f="R"
+class(f)
+typeof(f)
+demo=242L
+class(demo)
+typeof(demo)
+

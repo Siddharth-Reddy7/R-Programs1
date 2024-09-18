4e12624c514aa9b37e8e06c2cbb40c1d23f34532
Siddharth-Reddy7 <siddarthreddy8310@gmail.com>
2024-09-18 09:08:52 +0530

    Add files via upload

---
+++ operators.R
@@ -0,0 +1,13 @@
+{
+num1=as.integer(readline("enter the value"))
+num2=as.integer(readline("enter the value"))
+}
+paste("sum: ",num1+num2)
+paste("Difference: ",num1-num2)
+paste("Product: ",num1*num2)
+paste("Division: ",num1/num2)
+paste("Remainder: ",num1%%num2)#modulus
+paste("Power: ",num1^num2)
+paste("quotient:", num1%/%num2)#integer division
+
+
